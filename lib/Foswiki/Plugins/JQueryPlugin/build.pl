33b161d231e2fdf22827577304b258a56cfdf715
Jan Krüger <krueger@modell-aachen.de>
2012-06-29 16:07:42 +0200

    Release 4.42modac1

--- lib/Foswiki/Plugins/JQueryPlugin/build.pl
+++ lib/Foswiki/Plugins/JQueryPlugin/build.pl
@@ -7,6 +7,11 @@ use Foswiki::Contrib::Build;
 # Create the build object
 $build = new Foswiki::Contrib::Build('JQueryPlugin');
 
+$build->{UPLOADTARGETWEB} = 'Extensions';
+$build->{UPLOADTARGETPUB} = 'http://extensions.open-quality.com/pub';
+$build->{UPLOADTARGETSCRIPT} = 'http://extensions.open-quality.com/bin';
+$build->{UPLOADTARGETSUFFIX} = '';
+
 # Build the target on the command line, or the default target
 $build->build( $build->{target} );
 
