6266c92aca6f37db14f30198de147fbb9d7b0125
Jan Krüger <krueger@modell-aachen.de>
2012-08-16 13:25:05 +0200

    Release 4.42modac4

--- lib/Foswiki/Plugins/JQueryPlugin.pm
+++ lib/Foswiki/Plugins/JQueryPlugin.pm
@@ -20,7 +20,7 @@ use vars qw(
 );
 
 $VERSION           = '$Rev$';
-$RELEASE           = '4.42modac3';
+$RELEASE           = '4.42modac4';
 $SHORTDESCRIPTION  = 'jQuery <nop>JavaScript library for Foswiki';
 $NO_PREFS_IN_TOPIC = 1;
 
