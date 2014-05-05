fc7fb53b46d11d13a4124ad9b71aca15522e4bcd
Jan Krüger <krueger@modell-aachen.de>
2014-05-05 17:00:45 +0200

    TextboxList: bump JS version
    Ticket: none

--- lib/Foswiki/Plugins/JQueryPlugin/TEXTBOXLIST.pm
+++ lib/Foswiki/Plugins/JQueryPlugin/TEXTBOXLIST.pm
@@ -28,7 +28,7 @@ sub new {
     my $this = bless(
         $class->SUPER::new(
             name     => 'TextboxList',
-            version  => '1.01modac3',
+            version  => '1.01modac4',
             author   => 'Michael Daum',
             homepage => 'http://foswiki.org/Extensions/JQueryPlugin',
             css      => ['jquery.textboxlist.css'],
